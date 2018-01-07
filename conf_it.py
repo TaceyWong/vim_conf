331fb8acbe2c9e21c0243827bbcd9afee6e7f7b9
Tacey Wong <tacey_wong@163.com>
2018-01-07 14:35:38 +0800

    添signal捕捉，退出友好

--- conf_it.py
+++ conf_it.py
@@ -1,10 +1,18 @@
 # coding:utf-8
 
 import os
+import sys
 import shutil
+import signal
 
+logo_text = """__      ___                  _____             __
+\ \    / (_)                / ____|           / _|
+ \ \  / / _ _ __ ___ ______| |     ___  _ __ | |_
+  \ \/ / | | '_ ` _ \______| |    / _ \| '_ \|  _|
+   \  /  | | | | | | |     | |___| (_) | | | | |
+    \/   |_|_| |_| |_|      \_____\___/|_| |_|_|  v0.01
 
-
+"""
 
 def get_color_dir():
     path_template = "/usr/share/vim/vim{version}/colors"
@@ -23,11 +31,12 @@ def creat_conf_dir():
     try:
         shutil.rmtree(conf_dir)
     except Exception as e:
-        pass
+        print e
     try:
         os.makedirs(conf_dir)
     except Exception as e:
         print "Mkdir:",e
+        return None
     return conf_dir
 
 
@@ -38,12 +47,42 @@ def copy_conf(color_dir,conf_dir):
     conf_path = os.path.join(conf_dir,conf_sc)
     print color_path
     print conf_path
-    shutil.copyfile(color_sc,color_path)
-    shutil.copyfile(conf_sc,conf_path)
+    try:
+        shutil.copyfile(color_sc,color_path)
+        shutil.copyfile(conf_sc,conf_path)
+    except Exception as e:
+        print e
+        sys.exit(3)
 
 
+def rake():
+    print logo_text
+    ask = raw_input("This Will Remove all Your Configs for vim\n"
+                    "Enter->Continue\n"
+                    "Ctr-C->Exit\n:")
+    print("-Start-")
+    print("-To Get Color Dir")
+    color_dir = get_color_dir()
+    if not color_dir:
+        print("-Sorry,Cannot Find Vim-")
+        sys.exit(1)
+    print(color_dir)
+    print("-To Creat .vim Dir-")
+    conf_dir = creat_conf_dir()
+    if not conf_dir:
+        print("-Sorry,Failed to Create .vim Dir-")
+        sys.exit(2)
+    copy_conf(color_dir,conf_dir)
+    print("-Done-")
+
+def sig_service(func):
+    def handle_sig(sig,frame):
+        if int(sig) == 2:
+            print("Bye Bye!")
+            sys.exit(0)
+    signal.signal(signal.SIGTERM, handle_sig)
+    signal.signal(signal.SIGINT, handle_sig)
+    func()
 
 if __name__ == "__main__":
-    color_dir = get_color_dir()
-    conf_dir = creat_conf_dir()
-    copy_conf(color_dir,conf_dir)
+    sig_service(rake)
